b1cbee2467dc48baa488659e80c56f5c19ba3f02
e-d-i <e.theiss@hotmail.de>
2023-01-09 22:23:35 +0100

    6th url test

--- pages/chinesisch/contactUs.js
+++ pages/chinesisch/contactUs.js
@@ -26,7 +26,7 @@ function ContactUs() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await fetch("https://www.e-t-consulting.com/kontakt/api/mail", {
+      await fetch("https://www.e-t-consulting.com/api/mail", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
